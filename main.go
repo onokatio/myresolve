a15a8a99b5709c53cb60a0f8eb4d46a40e090cc2
onokatio <onokatio@gmail.com>
2020-05-05 01:30:25 +0900

    add DNS header struct

--- main.go
+++ main.go
@@ -1,6 +1,54 @@
 package main
 
 import "fmt"
+import "net"
+
+type DNSHeader struct {
+	ID int8 // random dns id
+	QR bool // question == 1, answer == 1
+	Opcode [4]bool // type of question
+	        // 0 == a standard query (QUERY)
+		// 1 == an inverse query (IQUERY)
+		// 2 == a server status request (STATUS)
+		// 3-15 == reserved for future use
+	AA bool // privilege. authority answer == 1, other ==0
+	TC bool // TrunCation notify bit
+	RD bool // Recursion query bit
+	RA bool // Recursion available bit
+	Z bool  // Reserved for future use.  Must be zero.
+	RCODE [4]bool // Respon code.
+	              // 0 == No error condition
+		      // 1 == Format error
+		      // 2 == Server failure
+		      // 3 == Name Error(Name not found from authority)
+		      // 4 == Not Implemented
+		      // 5 == Refused
+		      // 6-15 == Reserved for future use
+	QDCOUNT int16
+	ANCOUNT int16
+	NSCOUNT int16
+	ARCOUNT int16
+}
+
+type DNSQuestion struct {
+}
+
+type DNSAnswer struct {
+}
+
+type DNSAuthority struct {
+}
+
+type DNSAdditional struct {
+}
+
+type DNSPacket struct {
+	Header DNSHeader
+	Question DNSQuestion
+	Answer []DNSAnswer
+	Authority []DNSAuthority
+	Additional []DNSAdditional
+}
 
 func main() {
 	fmt.Println("Hello, onokatio full DNS resolver.")
