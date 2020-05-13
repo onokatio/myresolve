2db3b59f7d8435b2da6fbe2f19e579a5a3e7fef2
onokatio <onokatio@gmail.com>
2020-05-13 21:27:00 +0900

    successfully get response

--- main.go
+++ main.go
@@ -99,17 +99,16 @@ func main() {
 
 	rawpacket := new(bytes.Buffer)
 
-	binary.Write(rawpacket, binary.LittleEndian, int16(0x000f))
-	binary.Write(rawpacket, binary.LittleEndian, int8(0x00 | 0x00<<1 | 0x00<<2 | 0x00<<3 | 0x00<<4 | 0x00<<5 | 0x00<<6 | 0x00<<7)) // ID + Opcode + RD
-	binary.Write(rawpacket, binary.LittleEndian, int8(0x00 | 0x00<<1 | 0x00<<2 | 0x00<<3 | 0x00<<4 | 0x00<<5 | 0x00<<6 | 0x00<<7)) // RA + Z + RCODE
-	binary.Write(rawpacket, binary.LittleEndian, int16(0x0001))
-	binary.Write(rawpacket, binary.LittleEndian, int16(0x0000))
-	binary.Write(rawpacket, binary.LittleEndian, int16(0x0000))
-	binary.Write(rawpacket, binary.LittleEndian, int16(0x0000))
-	err := binary.Write(rawpacket, binary.LittleEndian, []byte{0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00} )
-	if err != nil {
-		panic(err)
-	}
+	binary.Write(rawpacket, binary.BigEndian, int16(0x000f))
+	binary.Write(rawpacket, binary.BigEndian, int8(0x00 | 0x00<<1 | 0x00<<2 | 0x00<<3 | 0x00<<4 | 0x00<<5 | 0x00<<6 | 0x00<<7)) // ID + Opcode + RD
+	binary.Write(rawpacket, binary.BigEndian, int8(0x00 | 0x00<<1 | 0x00<<2 | 0x00<<3 | 0x00<<4 | 0x00<<5 | 0x00<<6 | 0x00<<7)) // RA + Z + RCODE
+	binary.Write(rawpacket, binary.BigEndian, int16(0x0001))
+	binary.Write(rawpacket, binary.BigEndian, int16(0x0000))
+	binary.Write(rawpacket, binary.BigEndian, int16(0x0000))
+	binary.Write(rawpacket, binary.BigEndian, int16(0x0000))
+	binary.Write(rawpacket, binary.BigEndian, []byte{0x06, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d, 0x00} )
+	binary.Write(rawpacket, binary.BigEndian, int16(0x0001))
+	binary.Write(rawpacket, binary.BigEndian, int16(0x0001))
 
 	fmt.Printf("%x", rawpacket.Bytes())
 	conn.Write(rawpacket.Bytes())
